ff5113d41c1f80f9edd2dddd20a57d58eb49af04
John Kelly <john@backupify.com>
2011-06-06 08:53:33 -0400

    Updated the gem so you can configure it in an initializer

--- lib/marketo.rb
+++ lib/marketo.rb
@@ -5,3 +5,13 @@ require "openssl/digest"
 require "marketo/client"
 require "marketo/interface"
 require "marketo/lead"
+
+module Marketo
+  class << self
+    attr_accessor :access_key, :secret_key
+  end
+
+  def self.configure
+    yield self
+  end
+end
